9a526c5188a808de57ebff06104c7383276feb33
trevk <trev@candidsoftware.com>
2020-08-31 17:53:43 +0100

    trying to get fixed banner working

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,33 @@
 	<div class="absolute top-0 left-0 right-0 bottom-0">
 		<NavMobile v-if="mobileView" :loggedIn="loggedIn" :showNav="showNav"></NavMobile>
 		<div id="container"
-			 class="relative rounded-lg m-4 z-10 bg-white transition ease-in duration-500"
+			 class=" m-4 mt-0 z-10 bg-white transition ease-in duration-500"
 			 :class="{'slideContainer':showNav}">
-			<nav id="banner"
-				 class=" items-center bg-blue-800 p-3 pt-5 rounded-t-lg h-24 md:h-40 z-20 ver"
-				 :class="mobileView ? 'bannerMobile' : 'banner'"
-			>
-				<font-awesome-icon
-					v-if="mobileView" @click="toggleShowNav" icon="bars"
-					class="text-white text-3xl cursor-pointer inline-block"
-				/>
-				<router-link id="logo" to="/"
-					 exact-active-class=""
-					 class=" mt-1 ml-5 w-3/4 sm:w-2/3 lg:w-1/2 xl:w-1/2 inline-block visited:no-underline"
-				>
-					<img
-						alt="Adur Boat Sales"
-						class=""
-						contain
-						src="@/assets/images/adurboatsales.png"
-						transition="scale-transition"
-					/>
-				</router-link>
-				<NavRegular v-if="!mobileView" :loggedIn="loggedIn"></NavRegular>
-			</nav>
-			<router-view id="content" class="flex-column justify-center rounded-b-lg"/>
+      <div id="navcontainer" class="fixed top-0 pt-2 pr-4 h-24 md:h-40 z-20 ">
+        <nav id="banner"
+              class=" rounded-t-lg h-24 md:h-40"
+             :class="mobileView ? 'bannerMobile' : 'banner'"
+        >
+          <font-awesome-icon
+              v-if="mobileView" @click="toggleShowNav" icon="bars"
+              class="text-white text-3xl cursor-pointer inline-block"
+          />
+          <router-link id="logo" to="/"
+                       exact-active-class=""
+                       class=" mt-1 ml-5 w-3/4 sm:w-2/3 lg:w-1/2 xl:w-1/2 inline-block visited:no-underline"
+          >
+            <img
+                alt="Adur Boat Sales"
+                class="mt-4 rounded-top-lg"
+                contain
+                src="@/assets/images/adurboatsales.png"
+                transition="scale-transition"
+            />
+          </router-link>
+          <NavRegular v-if="!mobileView" :loggedIn="loggedIn"></NavRegular>
+        </nav>
+      </div>
+      <router-view id="content" class="pt-2 mt-24 md:mt-40 flex-column justify-center rounded-b-lg"/>
 		</div>
 	</div>
 </template>
@@ -91,6 +93,9 @@
 		height: 100%;
 		background-color: #ddd;
 	}
+  #navcontainer {
+    background-color: #ddd;
+  }
 	.banner {
 		background-image: url("./assets/images/sunset-over-yachts.jpg");
 		background-position-y: 80%;
@@ -99,9 +104,9 @@
 	}
 	.bannerMobile {
 		background-image: url("./assets/images/sunset-over-yachts.jpg");
-		background-position-y:50%;
+		background-position-y:70%;
     background-position-x:50%;
-		background-size: 120%;
+		background-size: 180%;
 	}
 	#content {
 		background-color: #88adc7;
